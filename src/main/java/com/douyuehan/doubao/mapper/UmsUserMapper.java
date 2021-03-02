79bf249aa79617070c76e6a053591145bdaab9c1
bing <bing_yu2001@163.com>
2021-03-02 15:25:10 +0800

    实现用户注册功能

---
+++ src/main/java/com/douyuehan/doubao/mapper/UmsUserMapper.java
@@ -0,0 +1,14 @@
+package com.douyuehan.doubao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.douyuehan.doubao.model.entity.UmsUser;
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author bing  @create 2021/3/2-12:30 下午
+ */
+@Repository
+public interface UmsUserMapper extends BaseMapper<UmsUser> {
+
+}
+
